e54e035d8800ba224ef5fb5ab87aeb3fcfc50139
José Luis <alagunasalahaddin@live.com>
2019-03-29 19:38:35 +0300

    Tweaks-

--- src/main/java/edu/anadolu/similarities/BM25F.java
+++ src/main/java/edu/anadolu/similarities/BM25F.java
@@ -2,15 +2,16 @@ package edu.anadolu.similarities;
 
 import org.apache.lucene.search.similarities.ModelBase;
 
-/** 
+/**  TODO: Review
  * This class implements a parameterized BM25  weighting model.
  **/
 
 public class BM25F extends ModelBase {
 
-    private final float k1;
-    private final float b;
+    private final double k_1;
+    private final double b;
 
+    // TODO: Review
     /**
      * BM25 with the supplied parameter values.
      * @param k1 Controls non-linear term frequency normalization (saturation).
@@ -18,6 +19,11 @@ public class BM25F extends ModelBase {
      * @throws IllegalArgumentException if {@code k1} is infinite or negative, or if {@code b} is
      *         not within the range {@code [0..1]}
      */
+
+    public BM25F(double k_1, double b) {
+        this.k_1 = k_1;
+        this.b = b;
+    }
     
     @Override
     public String toString(){
